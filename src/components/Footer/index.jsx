2872f5fddb312474e3f9e6849c9d2a52d8b0ff57
Raymond <ray@Raymonds-MacBook-Pro.local>
2025-06-16 23:29:48 -0700

    styled footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,14 +2,23 @@ import { Box, Typography, Link } from '@mui/material';
 
 export default function Footer() {
   return (
-    <Box component="footer" sx={{ py: 2, textAlign: 'center', }}>
+    <Box 
+      component="footer" 
+      sx={{ 
+        width: '100%',
+        py: 2, 
+        textAlign: 'center', 
+        backgroundColor: theme => theme.palette.secondary.main,
+        color: theme => theme.palette.primary.main,
+      }}
+    >
       <Typography variant="body2">
         © {new Date().getFullYear()} Alvaro's Auto Repair. All rights reserved.
       </Typography>
 
       <Typography variant="caption">
         Made by{' '}
-        <Link href="https://antbyte.netlify.app/" target="_blank" rel="noopener" underline="hover">
+        <Link href="https://antbyte.netlify.app/" target="_blank" rel="noopener" underline="hover" sx={{color:theme => theme.palette.third.main }}>
           AntByte
         </Link>
       </Typography>
